0ee0a5ba3c4c2f3b6a0d04851a677175acd7a332
arthurvanl <arthurvanleeuwen@hotmail.nl>
2023-03-18 10:34:45 +0100

    :white_check_mark: feat: add tests - close #12

---
+++ src/tests/schema.test.ts
@@ -0,0 +1,52 @@
+import { describe, test, expect } from "vitest";
+import { SchemaBuilder, SchemaPropertyBuilder } from "../builders/Schema.js";
+
+describe('Schema test builder', () => {
+    test('Expected to build a schema with 2 properties', () => {
+
+        const schema = new SchemaBuilder()
+            .setName('Test')
+            .setType('object')
+            .addProperty(
+                new SchemaPropertyBuilder()
+                    .setDeprecated(true)
+                    .setName('test')
+                    .addEnum(1)
+                    .addEnum(2)
+                    .setPropertyType('integer')
+                    .setMinLength(2)
+                    .setFormat('binary')
+            )
+            .addProperty(new SchemaPropertyBuilder()
+                .setDeprecated(false)
+                .setName('name')
+                .setPropertyType('string')
+                .setMaxLength(40)
+                .setMinLength(5)
+                .setExample('Arthur van Leeuwen')
+            )
+
+        expect(schema.toJSON()).toEqual(
+            {
+                name: 'Test',
+                type: 'object',
+                properties: {
+                    test: {
+                        format: 'binary',
+                        minLength: 2,
+                        enum: [1, 2],
+                        deprecated: true,
+                        type: 'integer'
+                    },
+                    name: {
+                        maxLength: 40,
+                        minLength: 5,
+                        example: 'Arthur van Leeuwen',
+                        type: 'string',
+                        deprecated: false
+                    }
+                }
+            }
+        )
+    })
+})
