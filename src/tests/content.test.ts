713987584b053f9b55f8810bd248bb90d4ca44a1
arthurvanl <info@arthurvanl.nl>
2023-03-18 12:28:20 +0100

    :white_check_mark: chore: update schema - close #13

--- src/tests/content.test.ts
+++ src/tests/content.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test } from "vitest";
 import { ContentBuilder } from "../builders/Content.js";
-import { SchemaBuilder } from "../builders/Schema.js";
+import { SchemaBuilder, SchemaPropertyBuilder } from "../builders/Schema.js";
 
 describe('Content test builder', () => {
     test('Expected to build media content object with schema', () => {
@@ -8,9 +8,17 @@ describe('Content test builder', () => {
         const mediaContent = new ContentBuilder()
         .setName('application/json')
         .setSchema(new SchemaBuilder()
-            .setName('message')
-            .setType('string')
+            .setName('user')
+            .setType('object')
             .setRef('#/components/responses/user/post')
+            .addProperty(new SchemaPropertyBuilder()
+                .setDeprecated(false)
+                .setName('name')
+                .setType('string')
+                .setMaxLength(40)
+                .setMinLength(5)
+                .setExample('Arthur van Leeuwen')
+            )
         )
         .setExample({
             message: 'What a nice message!'
@@ -22,9 +30,18 @@ describe('Content test builder', () => {
                 message: 'What a nice message!'
             },
             schema: {
-                name: 'message',
-                type: 'string',
+                name: 'user',
+                type: 'object',
                 ref: '#/components/responses/user/post',
+                properties: {
+                    name: {
+                        type: 'string',
+                        maxLength: 40,
+                        minLength: 5,
+                        example: 'Arthur van Leeuwen',
+                        deprecated: false
+                    }
+                }
             }
         })
     });
