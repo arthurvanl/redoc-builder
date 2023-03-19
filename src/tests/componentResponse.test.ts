96db066c45398f602bbc66132e76148c9042ef6a
arthurvanl <arthurvanleeuwen@hotmail.nl>
2023-03-19 15:04:50 +0100

    :white_check_mark: feat: add `componentResponse` test - close #8

---
+++ src/tests/componentResponse.test.ts
@@ -0,0 +1,59 @@
+import { describe, expect, test } from "vitest";
+import { ComponentResponseBuilder } from "../builders/ComponentResponse.js";
+import { ContentBuilder } from "../builders/Content.js";
+import { SchemaBuilder, SchemaPropertyBuilder } from "../builders/Schema.js";
+
+describe('ComponentResponse test builder', () => {
+
+    test('Expected to build ComponentResponse with GET method', () => {
+
+        const component = new ComponentResponseBuilder()
+        .setName('200')
+        .setDescription('Success')
+        .addContent(new ContentBuilder()
+            .setName('application/json')
+            .setSchema(new SchemaBuilder()
+                .setName('user')
+                .setType('object')
+                .setRef('#/components/responses/user/post')
+                .addProperty(new SchemaPropertyBuilder()
+                    .setDeprecated(false)
+                    .setName('name')
+                    .setType('string')
+                    .setMaxLength(40)
+                    .setMinLength(5)
+                    .setExample('Arthur van Leeuwen')
+                )
+            )
+            .setExample({
+                message: 'What a nice message!'
+            })
+        );
+
+        expect(component.toJSON()).toEqual({
+            name: '200',
+            description: 'Success',
+            content: {
+                'application/json': {
+                    example: {
+                        message: 'What a nice message!'
+                    },
+                    schema: {
+                        name: 'user',
+                        type: 'object',
+                        ref: '#/components/responses/user/post',
+                        properties: {
+                            name: {
+                                type: 'string',
+                                maxLength: 40,
+                                minLength: 5,
+                                example: 'Arthur van Leeuwen',
+                                deprecated: false
+                            }
+                        }
+                    }
+                }
+            }
+        })
+    });
+})
